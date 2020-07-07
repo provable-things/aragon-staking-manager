a9eb99ecb6bbc910a76390ecb4a2d253aa7c78b1
allemanfredi <alle.manfredi@gmail.com>
2020-07-07 08:12:25 +0200

    chore(app): adds side panel

--- app/src/components/Wrapper.js
+++ app/src/components/Wrapper.js
@@ -1,22 +1,58 @@
 import React, { Fragment, useState } from 'react'
-import { Box, Button, Field, GU, Tabs, textStyle } from '@aragon/ui'
-import { useAragonApi } from '@aragon/api-react'
+import styled from 'styled-components'
+import { Box, Button, Field, GU, Info, TextInput, textStyle } from '@aragon/ui'
+import PropTypes from 'prop-types'
 
 const Wrapper = (_props) => {
-  const { amount, selected, onChangeAmount, onChangeSelected, onClick } = _props
+  const { action, onClick } = _props
+
+  const [amount, setAmount] = useState('')
 
   return (
     <Fragment>
-      <Field label="Enter the amount here:">
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => onChangeAmount(e.target.value)}
-        />
-      </Field>
-      <Button onClick={() => onClick()}>ciaoi</Button>
+      <Info
+        title="ACTION"
+        css={`
+          width: 100%;
+          margin-top: ${2 * GU}px;
+        `}
+      >
+        {`This action will ${
+          action === 'Wrap' ? 'create' : 'burn'
+        } tokens and transfer them to the transaction sender`}
+      </Info>
+      <WrapperField>
+        <Field
+          label="Enter the amount here:"
+          required
+          css={`
+            margin-top: ${3 * GU}px;
+          `}
+        >
+          <TextInput.Number
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            wide
+            min={0}
+            step="any"
+            required
+          />
+        </Field>
+      </WrapperField>
+      <Button onClick={() => onClick()}>{action}</Button>
     </Fragment>
   )
+}
+
+const WrapperField = styled.div`
+  label {
+    width: 100% !important;
+  }
+`
+
+Wrapper.propTypes = {
+  action: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Wrapper
