3fc58c6784f1d9c2449c539959c01a6067edab79
allemanfredi <alle.manfredi@gmail.com>
2020-07-10 08:40:30 +0200

    ref(app): formats

--- app/src/reducer.js
+++ app/src/reducer.js
@@ -16,10 +16,12 @@ const reducer = (_state) => {
 
   return {
     ..._state,
-    lockedWraps: lockedWraps ? lockedWraps.filter(
-      ({ amount, lockDate, lockTime }) =>
-        amount !== 0 && lockDate !== 0 && lockTime !== 0
-    ) : [],
+    lockedWraps: lockedWraps
+      ? lockedWraps.filter(
+          ({ amount, lockDate, lockTime }) =>
+            amount !== 0 && lockDate !== 0 && lockTime !== 0
+        )
+      : [],
   }
 }
 
